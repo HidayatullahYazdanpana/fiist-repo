9bcc02532433301df8d14fd188d493a10404cd5c
Hidayatullah Yazdanpana <hidayatullah.uni@gmail.com>
2026-06-30 22:52:57 +0430

    Add new_check function placeholder
    I add a new func

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ def check_two_list(list_one,list_two):
             if i == j:
                 print(f"i : {i}\nj : {j}")
 
-
+def new_check():
+    pass
 if __name__ == '__main__':
     check_two_list([1,2,3],[1,2,3,4])
