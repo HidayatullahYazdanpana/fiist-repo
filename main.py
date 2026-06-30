06a6e59d01b24fe87fdfb737384a6f1f705d1a28
HidayatullahYazdanpana <hidayatullah.uni@gmail.com>
2026-06-30 22:41:51 +0430

    Second Change

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def check_two_list(list_one,list_two):
+    for i in list_one:
+        for j in list_two:
+            if i == j:
+                print(f"i : {i}\nj : {j}")
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    check_two_list([1,2,3],[1,2,3,4])
